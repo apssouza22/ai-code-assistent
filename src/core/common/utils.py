6f8d19c7a36eb7ee0b7f6fff4b12ec2bd1a44ee5
alexsandro.souza <alexsandro.souza@workday.com>
2026-03-20 21:08:00 +0100

    setup

--- src/core/common/utils.py
+++
@@ -1,4 +0,0 @@
-def format_tool_output(tool_name: str, content: str) -> str:
-    """Format tool output in XML format."""
-    tag_name = f"{tool_name}_output"
-    return f"<{tag_name}>\n{content}\n</{tag_name}>"
--- src/core/todo/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = [
-    "TodoManager",
-    "BatchTodoActionHandler",
-]
-from src.core.todo.todo_manager import TodoManager
-from src.core.todo.todo_handler import BatchTodoActionHandler
